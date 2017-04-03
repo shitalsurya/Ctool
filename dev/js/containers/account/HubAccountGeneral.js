671edb0167a89037ba8ec4252586744835211c28
SNEHA <SNEHAK2110ROX@GMAIL.COM>
2017-04-03 15:35:35 +0530

    onMouseOver Edit

--- dev/js/containers/account/HubAccountGeneral.js
+++ dev/js/containers/account/HubAccountGeneral.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Nav,NavItem } from 'react-bootstrap';
+import { Nav, NavItem, FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 require('../../../scss/style.scss');
 
 class HubAccountTPOA extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.state = {
+          editFlag : false,
+          hiddenFlag : true,
+        }
     }
 
+    onMouseEdit() {
+      this.setState({hiddenFlag : !this.state.hiddenFlag});
+      // console.log(this.state.hiddenFlag);
+    }
 
     render() {
 
@@ -17,8 +25,17 @@ class HubAccountTPOA extends React.Component {
         return (
                  <div className="controls-container">
                    <div className="rec">
-                   <span>Commercial Information</span>
+                     <span>Commercial Information</span>
+                     <FormGroup>
+                        <InputGroup className={this.state.hiddenFlag ? "hide-icon" : false} onMouseOver={this.onMouseEdit.bind(this)} onMouseOut={this.onMouseEdit.bind(this)}>
+                            <FormControl name="frm1" type="text" disabled={this.state.editFlag ? false : "disabled"} />
+                            <InputGroup.Addon>
+                              <Glyphicon glyph="pencil" onClick={ () => this.setState({editFlag : true})}/>
+                            </InputGroup.Addon>
+                        </InputGroup>
+                      </FormGroup>
                    </div>
+                   <div className="rec"></div>
                  </div>
         )
     }
