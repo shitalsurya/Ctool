da85d3e7b7d90c997184b11776349dcb45e6bede
SNEHA <SNEHAK2110ROX@GMAIL.COM>
2017-04-20 17:37:23 +0530

    MO Routing Page development

--- dev/js/containers/account/HubAccountMORouting.js
+++ dev/js/containers/account/HubAccountMORouting.js
@@ -2,30 +2,97 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Nav,NavItem } from 'react-bootstrap';
-
+import Select from 'react-select';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
+import { Form, FormGroup, Col, Row, FormControl, ControlLabel, Grid,ButtonGroup,Button,Modal,Label, OverlayTrigger, Popover, ButtonToolbar } from 'react-bootstrap';
+import AddDedicatedMORouting from './HubAccountMODedicated';
+import AddParsedMORouting from './HubAccountMOParsed';
+import AddMORouting from './HubAccountMORoutingAdd';
+import InlineEdit from './../common/components/InlineEdit';
+import DeleteRowLink from './../common/components/DeleteRow';
 require('../../../scss/style.scss');
 
 class HubAccountMORouting extends React.Component {
     constructor(props, context) {
         super(props, context);
-    }
+        this.state = {
+          groupById : 'id',
+          data : [
 
+          ],
+          showAddDedicated : false,
+          showAddParsed : false,
+        }
+    }
 
     render() {
 
-
         return (
-                 <div className="tabs-container">
-                   <div className="rec">
-                   <span>MO Routing</span>
-                   </div>
-                 </div>
+           <div className="tabs-container">
+             <Grid fluid={true}>
+
+               <Row className="show-grid">
+                 <Col componentClass={ ControlLabel } md = { 12 } >
+                   Standard MO Routing
+                 </Col>
+               </Row>
+
+               <Row className="show-grid">
+                 <Col mdHidden md={ 6 } >
+                   <ButtonGroup justified>
+                     <Button href="#" className="grp-btn" onClick={() => this.setState({showAddDedicated : true, showAddParsed : false}) }>
+                       <span className="add-icon"></span>
+                       <span>Add Dedicated Routings</span>
+                     </Button>
+                     <Button href="#" className="grp-btn" onClick={() => this.setState({showAddDedicated : true, showAddParsed : true}) }>
+                       <span className="add-icon"></span>
+                       <span>Add Parsed Routings</span>
+                     </Button>
+                   </ButtonGroup>
+                 </Col>
+               </Row>
+
+               <Row className="show-grid">
+                 <Col md={ 12 }>
+                   <BootstrapTable data={this.state.data} >
+                     <TableHeaderColumn isKey={ true } hidden dataField={this.state.groupById}>ID</TableHeaderColumn>
+                     <TableHeaderColumn dataField='SMSC'>SMSC</TableHeaderColumn>
+                     <TableHeaderColumn dataField='serviceNo' dataFormat={ this.dataFormatter.bind(this) } formatExtraData={ 'serviceNo' } >Service Number</TableHeaderColumn>
+                     <TableHeaderColumn dataField='criteria' dataFormat={ this.dataFormatter.bind(this) } formatExtraData={ 'criteria' } >Criteria</TableHeaderColumn>
+                     <TableHeaderColumn dataField='returnTPDA' dataFormat={ this.dataFormatter.bind(this) } formatExtraData={ 'returnTPDA' } >Return TPDA</TableHeaderColumn>
+                     <TableHeaderColumn dataField='delete' dataFormat={ this.deleteDataFormatter.bind(this) } formatExtraData={ 'delete' } ></TableHeaderColumn>
+                   </BootstrapTable>
+                 </Col>
+               </Row>
+
+             </Grid>
+
+             {/*<AddDedicatedMORouting showAdd={this.state.showAddDedicated} close={this.close.bind(this)}/>
+           <AddParsedMORouting showAdd={this.state.showAddParsed} close={this.close.bind(this)}/>*/}
+
+              <AddMORouting showAdd={this.state.showAddDedicated} showParsed={this.state.showAddParsed} close={this.close.bind(this)}/>
+
+           </div>
         )
     }
 
     componentWillReceiveProps(nextProps) {
 
-      }
+    }
+
+    close() {
+      this.setState({ showAddDedicated: false, showAddParsed: false});
+    }
+
+    deleteDataFormatter(cell, row,field,index) {
+      return (
+          <DeleteRowLink currentRow={this.currentRow.SMSCOp}/>
+      )
+    }
+
+    dataFormatter(cell, row,field,index) {
+      return <InlineEdit type='text' value={cell} onSave={this.handleInlineEditChange.bind(this)}  />
+    }
 
 }
 
---
+++ dev/js/containers/account/HubAccountMORoutingAdd.js
@@ -0,0 +1,215 @@
+import React from 'react';
+import { Form, FormGroup, Col, Row, FormControl, ControlLabel, Grid,ButtonGroup,Button,Modal,Label } from 'react-bootstrap';
+import Select from 'react-select';
+require('../../../scss/style.scss');
+import * as types from '../../containers/account/actions/accountActionTypes';
+
+class HubAccountMORoutingAdd extends React.Component {
+  constructor(props, context) {
+      super(props, context);
+        this.state={
+             MORouting : {},
+             modalHeading:'Add MO Routing',
+        }
+  }
+
+  handleModalChange(target, value){
+    var info = this.state.MORouting;
+    switch(target) {
+      case types.TPOA_PARSED_SMSC:
+        info.smsc = value.value;
+        break;
+      case types.TPOA_PARSED_SERVICE:
+        info.serviceNo = value.target.value;
+        break;
+      case types.TPOA_PARSED_RETURNED_TPDA:
+        info.returnTPDA = value.target.value;
+        break;
+      case types.TPOA_PARSED_CRITERIA:
+        info.criteria = value.value;
+        break;
+      case types.TPOA_PARSED_PFIELD:
+        info.parsedField = value.value;
+        break;
+      case types.TPOA_PARSED_KEYWORD_CRITERIA:
+        info.kwCriteria = value.value;
+        break;
+      case types.TPOA_PARSED_KEYWORD:
+        info.keyword = value.target.value;
+        break;
+    }
+    this.setState({TPOAinfo : info});
+  }
+
+  addRouting(){
+    console.log(this.state.MORouting);
+    this.setState({MORouting : []});
+    this.props.close();
+  }
+
+  close() {
+    this.setState({MORouting : []});
+    this.props.close();
+  }
+
+  render(){
+
+    const options = [
+      { value: 'SMSC1', label: 'SMSC1' },
+      { value: 'SMSC2', label: 'SMSC2' },
+      { value: 'SMSC3', label: 'SMSC3' },
+      { value: 'SMSC4', label: 'SMSC4' },
+      { value: 'SMSC5', label: 'SMSC5' },
+      { value: 'SMSC6', label: 'SMSC6' }
+    ];
+
+    const criteriaOp = [
+      { value: 'criteria1', label: 'criteria1' },
+      { value: 'criteria2', label: 'criteria2' },
+      { value: 'criteria3', label: 'criteria3' },
+      { value: 'criteria4', label: 'criteria4' }
+    ];
+
+    const parsedOp = [
+      { value: 'field1', label: 'field1' },
+      { value: 'field2', label: 'field2' },
+      { value: 'field3', label: 'field3' },
+      { value: 'field4', label: 'field4' },
+      { value: 'field5', label: 'field5' }
+    ];
+
+    const kwCriteriaOp = [
+      { value: 'keyword1', label: 'keyword1' },
+      { value: 'keyword2', label: 'keyword2' },
+      { value: 'keyword3', label: 'keyword3' },
+      { value: 'keyword4', label: 'keyword4' }
+    ];
+    return (
+      <Modal show={this.props.showAdd} onHide={this.close.bind(this)}>
+          <Modal.Header closeButton>
+              <Modal.Title>{this.state.modalHeading}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <Grid fluid={true}>
+                <Row className="show-grid">
+                  <Col componentClass={ ControlLabel } md={ 4 }>
+                    SMSC:
+                  </Col>
+                  <Col md={ 6 }>
+                    <Select
+                      name="smsc"
+                      placeholder="Select SMSC.."
+                      options={options}
+                      value={this.state.MORouting.smsc || ''}
+                      onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_SMSC)}
+                    />
+                  </Col>
+                  <Col mdHidden md={ 2 } />
+                </Row>
+                <Row className="show-grid">
+                    <Col componentClass={ ControlLabel } md={ 4 }>
+                      Service Number :
+                    </Col>
+                    <Col md={ 6 }>
+                      <FormControl
+                         type="text"
+                         name="serviceNo"
+                         value={this.state.MORouting.serviceNo || ''}
+                         onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_SERVICE)}
+                         placeholder="Enter Service Number.." />
+                    </Col>
+                    <Col mdHidden md={ 2 } />
+                </Row>
+                <Row className="show-grid">
+                    <Col componentClass={ ControlLabel } md={ 4 }>
+                      Returned TPDA :
+                    </Col>
+                    <Col md={ 6 }>
+                      <FormControl
+                         type="text"
+                         name="returnTPDA"
+                         value={this.state.MORouting.returnTPDA || ''}
+                         onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_RETURNED_TPDA)}
+                         placeholder="Enter Returned TPDA.." />
+                    </Col>
+                    <Col mdHidden md={ 2 } />
+                </Row>
+                <Row className="show-grid">
+                  <Col componentClass={ ControlLabel } md={ 4 }>
+                    TPDA Criteria:
+                  </Col>
+                  <Col md={ 6 }>
+                    <Select
+                      name="criteria"
+                      placeholder="Select Criteria.."
+                      options={criteriaOp}
+                      value={this.state.MORouting.criteria || ''}
+                      onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_CRITERIA)}
+                    />
+                  </Col>
+                  <Col mdHidden md={ 2 } />
+                </Row>
+                {this.props.showParsed &&
+                <div>
+                  <Row className="show-grid">
+                    <Col componentClass={ ControlLabel } md={ 4 }>
+                      Parsed Field:
+                    </Col>
+                    <Col md={ 6 }>
+                      <Select
+                        name="parsedField"
+                        placeholder="Select Parsed Field.."
+                        options={parsedOp}
+                        value={this.state.MORouting.parsedField || ''}
+                        onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_PFIELD)}
+                      />
+                    </Col>
+                    <Col mdHidden md={ 2 } />
+                  </Row>
+                  <Row className="show-grid">
+                    <Col componentClass={ ControlLabel } md={ 4 }>
+                      Keyword Criteria:
+                    </Col>
+                    <Col md={ 6 }>
+                      <Select
+                        name="kwCriteria"
+                        placeholder="Select Keyword Criteria.."
+                        options={kwCriteriaOp}
+                        value={this.state.MORouting.kwCriteria || ''}
+                        onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_KEYWORD_CRITERIA)}
+                      />
+                    </Col>
+                    <Col mdHidden md={ 2 } />
+                  </Row>
+                  <Row className="show-grid">
+                      <Col componentClass={ ControlLabel } md={ 4 }>
+                        Keyword :
+                      </Col>
+                      <Col md={ 6 }>
+                        <FormControl
+                           type="text"
+                           name="keyword"
+                           value={this.state.MORouting.keyword || ''}
+                           onChange={this.handleModalChange.bind(this,types.TPOA_PARSED_KEYWORD)}
+                           placeholder="Enter Keyword.." />
+                      </Col>
+                      <Col mdHidden md={ 2 } />
+                  </Row>
+                </div>
+                }
+              </Grid>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.addRouting.bind(this)}>Save</Button>
+            <Button onClick={this.close.bind(this)}>Close</Button>
+          </Modal.Footer>
+      </Modal>
+
+    );
+  }
+
+}
+
+export default HubAccountMORoutingAdd;
