19e0b9ffd03f55bc0c2f9b7352d9dc7bbc27cbcb
SNEHA <SNEHAK2110ROX@GMAIL.COM>
2017-05-03 15:32:15 +0530

    generic router, generic labelling of submenus

--- dev/js/containers/common/commonActionTypes.js
+++ dev/js/containers/common/commonActionTypes.js
@@ -1,20 +1,20 @@
 
 //Account module
-export const ACCOUNT_LIST='ACCOUNT_LIST';
-export const ACCOUNT_DETAILS='ACCOUNT_DETAILS';
-export const ACCOUNT_CREATE = 'ACCOUNT_CREATE';
-export const ACCOUNT_REAC = 'ACCOUNT_REAC';
-export const ACCOUNT_SPND = 'ACCOUNT_SPND';
-export const ACCOUNT_CLOSE = 'ACCOUNT_CLOSE';
+export const ACCOUNT_LIST='Accounts';
+export const ACCOUNT_DETAILS='Account Details';
+export const ACCOUNT_CREATE = 'Create Account';
+export const ACCOUNT_REAC = 'Reactivate Account';
+export const ACCOUNT_SPND = 'Suspend Account';
+export const ACCOUNT_CLOSE = 'Close Account';
 
 //Account module
 export const CONNECTIONS = 'CONNECTIONS';
 
 //Miscelleneous
-export const MISCELLENEOUS = 'MISCELLENEOUS';
+export const MISCELLENEOUS = 'Miscelleneous';
 export const TOOLBOX_SEARCH = 'TOOLBOX_SEARCH';
-export const USER_MANAGEMENT = 'USER_MANAGEMENT';
-export const COUNTRY_MANAGEMENT = 'COUNTRY_MANAGEMENT';
+export const USER_MANAGEMENT = 'User Management';
+export const COUNTRY_MANAGEMENT = 'Country Management';
 
 //Users
 export const MISC_USERS = 'MISC_USERS';
