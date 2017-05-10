3226a59ab388751d44459eb1e1a203aa2cf01693
SNEHA <SNEHAK2110ROX@GMAIL.COM>
2017-05-10 16:43:06 +0530

    multiSelect styling css

--- dev/js/containers/common/components/InlineEdit.js
+++ dev/js/containers/common/components/InlineEdit.js
@@ -234,7 +234,6 @@ export default class InlineEdit extends React.Component {
                   {
                     this.state.mselect &&
                       <Typeahead
-                        clearButton
                         defaultSelected = {this.state.selected}
                         labelKey="key"
                         multiple
