a3cc686d83fd16efdc3a731d3dbde83785a9216d
SNEHA <SNEHAK2110ROX@GMAIL.COM>
2017-05-24 11:00:42 +0530

    two files with css

--- dev/js/containers/common/wizardStepper.js
+++ dev/js/containers/common/wizardStepper.js
@@ -1,7 +1,8 @@
 import React from 'react';
-require('../../../scss/style.scss');
+// require('../../../scss/style.scss');
+import style from './WizardStepper.style.css';
 
-class Stepper extends React.Component {
+class WizardStepper extends React.Component {
   constructor( props, context ) {
     super( props, context );
   }
@@ -9,27 +10,27 @@ class Stepper extends React.Component {
 
   render() {
     return (
-      <div className="stepwizard breadcrumb-container">
-        <div className="stepwizard-row non-clickable">
-            <div className="stepwizard-step">
+      <div className={style.stepwizard + " " +style.breadcrumb_container}>
+        <div className={style.stepwizard_row + " non-clickable"}>
+            <div className={style.stepwizard_step}>
                 <button type="button" className={this.props.buttonStyle.commStyle} >
                   1
                 </button>
                 <p>Commercial Information</p>
             </div>
-            <div className="stepwizard-step">
+            <div className={style.stepwizard_step}>
                 <button type="button" className={this.props.buttonStyle.techStyle} >
                   2
                 </button>
                 <p>Customer Contacts</p>
             </div>
-            <div className="stepwizard-step">
+            <div className={style.stepwizard_step}>
                 <button type="button" className={this.props.buttonStyle.intrStyle} >
                   3
                 </button>
                 <p>Technical Details</p>
             </div>
-            <div className="stepwizard-step">
+            <div className={style.stepwizard_step}>
                 <button type="button" className={this.props.buttonStyle.revwStyle} >
                   4
                 </button>
@@ -42,4 +43,4 @@ class Stepper extends React.Component {
 
 }
 
-export default Stepper;
+export default WizardStepper;
