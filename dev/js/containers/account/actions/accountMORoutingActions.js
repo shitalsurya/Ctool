c7d8f114bf5ab361dca11d755d32170f9535dc96
shital <shita.suryavanshi@xoriant.com>
2017-06-01 22:30:23 +0530

    MT routing

---
+++ dev/js/containers/account/actions/accountMORoutingActions.js
@@ -0,0 +1,107 @@
+import * as types from './../../common/commonActionTypes';
+import axios from 'axios';
+import * as config from '../../../containers/common/config';
+import {httpRequest} from '../../../containers/common/commonAjaxActions';
+			export function getHubAcctMORoutingList(currentAcct) {
+				return function (dispatch,getState) {
+			    dispatch(getHubAcctMORoutingListRequest());
+					var request = {
+						url:config.getUrl('hub_accounts_tpoa')+'/'+currentAcct,
+						method:'GET',
+						successCallback:getHubAcctMORoutingListResponse,
+						failureCallback:getHubAcctMORoutingListResponse
+					};
+			    return httpRequest(dispatch,getState,request);
+				}
+			}
+			export function getHubAcctMORoutingListRequest() {
+					return {
+						type: types.GET_ACCT_MO_ROUTING_LIST_REQUEST
+					}
+				}
+				export function getHubAcctMORoutingListResponse(response) {
+						return {
+							type: types.GET_ACCT_MO_ROUTING_LIST_RESPONSE,
+							 payload: TPOA_Info
+						}
+					}
+					export function AddHubAccountMORouting(_newMOinfo) {
+							return function (dispatch,getState) {
+								dispatch(AddHubAccountMORoutingRequest());
+								var request = {
+									url:config.getUrl('hub_accounts')+'/MO/routing/'+_newMOinfo.customerid,
+									method:'POST',
+									data:_newMOinfo,
+									successCallback:AddHubAccountMORoutingResponse,
+									failureCallback:AddHubAccountMORoutingResponse
+								};
+								return httpRequest(dispatch,getState,request);
+							}
+						}
+					export function AddHubAccountMORoutingRequest() {
+							return {
+								type: types.ADD_ACCT_MO_ROUTING_LIST_REQUEST
+							}
+						}
+						export function AddHubAccountMORoutingResponse(response) {
+								return {
+									type: types.ADD_ACCT_MO_ROUTING_LIST_RESPONSE,
+									 payload:response
+								}
+							}
+							export function UpdateHubAccountMORouting(_updateParam,_TPOAinfo) {
+								var _url;
+								switch(_updateParam){
+									case "updateDefaultTPOA":
+										_url = config.getUrl('hub_accounts')+'/'+_TPOAinfo.customerid+'/defaulttpoa';
+									break;
+								}
+
+									return function (dispatch,getState) {
+										dispatch(UpdateHubAccountMORoutingRequest());
+										var request = {
+											url:_url,
+											method:'PUT',
+											data:_TPOAinfo,
+											successCallback:UpdateHubAccountMORoutingResponse,
+											failureCallback:UpdateHubAccountMORoutingResponse
+										};
+										return httpRequest(dispatch,getState,request);
+									}
+								}
+							export function UpdateHubAccountMORoutingRequest() {
+									return {
+										type: types.UPDATE_ACCT_MO_ROUTING_LIST_REQUEST
+									}
+								}
+								export function UpdateHubAccountMORoutingResponse(response) {
+										return {
+											type: types.UPDATE_ACCT_MO_ROUTING_LIST_RESPONSE,
+											 payload: response
+										}
+									}
+									export function DeleteHubAccountMORouting(currentAcct) {
+										return function (dispatch,getState) {
+											dispatch(DeleteHubAccountMORoutingRequest());
+											var request = {
+												url:config.getUrl('hub_accounts')+'/'+currentAcct.customerid+'/'+currentAcct.smscid+'/MORouting',
+												method:'DELETE',
+												data:currentAcct,
+												successCallback:DeleteHubAccountMORoutingResponse,
+												failureCallback:DeleteHubAccountMORoutingResponse
+											};
+											return httpRequest(dispatch,getState,request);
+										}
+									}
+
+									export function DeleteHubAccountMORoutingRequest() {
+										return {
+											type: types.DELETE_ACCT_MO_ROUTING_LIST_REQUEST
+										}
+									}
+									export function DeleteHubAccountMORoutingResponse(response) {
+										return {
+											type: types.DELETE_ACCT_MO_ROUTING_LIST_RESPONSE,
+											 payload: response
+										}
+									}
